2d5a405a58a9c4318881acd5b37f135cde66b58c
Apollo Blaze <srichandsureshrocks@gmail.com>
2024-09-23 12:52:15 +0530

    first commit

---
+++ weather.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+import matplotlib.pyplot as plt
+from meteostat import Point, Daily
+
+# Set time period
+city = Point(38.3498, -81.6326, 200)  # Latitude, Longitude, Elevation in meters
+
+# Set the date for which you want to retrieve the data
+start = datetime(2010, 6, 24)
+end = datetime(2010, 6, 24)
+
+# Get daily data for 2018
+data = Daily(city, start, end)
+data = data.fetch()
+print(data)
+
