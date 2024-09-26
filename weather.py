0d7948dfac71655e90b1991a359b5dc5a130c957
Apollo Blaze <srichandsureshrocks@gmail.com>
2024-09-26 19:40:25 +0530

    connected github to local project

--- weather.py
+++ weather.py
@@ -6,8 +6,8 @@ from meteostat import Point, Daily
 city = Point(38.3498, -81.6326, 200)  # Latitude, Longitude, Elevation in meters
 
 # Set the date for which you want to retrieve the data
-start = datetime(2010, 6, 24)
-end = datetime(2010, 6, 24)
+start = datetime(2023, 6, 24)
+end = datetime(2023, 6, 24)
 
 # Get daily data for 2018
 data = Daily(city, start, end)
